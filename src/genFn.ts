701a8f656dc17d3dce2308cf9368ea5ecfd97597
Marius <marius@example.com>
2022-07-14 11:21:52 +0300

    Type extends string

--- src/genFn.ts
+++ src/genFn.ts
@@ -48,9 +48,13 @@ console.log('rez112 ===', rez112);
 // add types or interfaces su product1 and warehouse1
 // create funcion addPostalAddress =>  postalAddress: 'Vilnius, Vokieciu st'
 
-const addId = (arg: number): [number, number] => [Math.random(), arg];
+const addId = <Type extends string | number>(arg: Type): [number, Type] => {
+  console.log([Math.random(), arg]);
+  return [Math.random(), arg];
+};
 
-const id1 = addId(15);
-const id2 = addId('Serbentautas');
-console.log('id1 ===', id1);
+const id1 = addId<number>(15);
+const id2 = addId<string>('Serbentautas');
+// const id3 = addId<{ title: string }>({ title: 'mr' });
+// console.log('id1 ===', id1, id2);
 // pataisyti addId kad ji veiktu su generic argumentu
